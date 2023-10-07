8b783dea24a64ba69da5e499fc28f3b2f9c159bc
donbadao <22314385+donbadao@users.noreply.github.com>
2023-10-07 10:42:46 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,35 +1,12 @@
 const TelegramBot = require('node-telegram-bot-api');
 const token = '6683681793:AAEtk8pBXlpHfqofpjDVKEvkzfhQQjYjlGo';
 const bot = new TelegramBot(token, { polling: true });
-
+const action = new require("./modules/actions.js");
 
 bot.onText(/\/hello/, (msg) => {
-    const chatId = msg.chat.id;
-    bot.sendMessage(chatId, "Hello World từ server Render");
+    action.info(msg);
 });
 
 bot.on('message', (msg) => {
-    const badWordsList = ['chó', 'dmcs', 'phản động', 'cc'];
-
-    const chatId = msg.chat.id;
-    const messageId = msg.message_id;
-    const text = msg.text;
-    // Chuyển tiếp toàn bộ tin nhắn đến USER_ID_DESTINATION
-    // bot.forwardMessage(chatId, chatId, msg.message_id); // Chuyển tiếp tin nhắn
-
-    const pattern = /^Trợ giúp:\s*(.+)$/;
-    const match = text.match(pattern);
-    if (match) {
-        const userName = msg.from.username || `${msg.from.first_name} ${msg.from.last_name}`;
-        const replyText = `[${userName}](tg://user?id=${msg.from.id}): Hệ thống đã ghi nhận yêu cầu của bạn`;
-        bot.sendMessage(chatId, replyText, { reply_to_message_id: messageId, parse_mode: 'Markdown' });
-    }
-
-    const containsBadWord = badWordsList.some(word => text.toLowerCase().includes(word.toLowerCase()));
-
-    if (containsBadWord) {
-        const userName = msg.from.username || `${msg.from.first_name} ${msg.from.last_name}`;
-        const replyText = `[${userName}](tg://user?id=${msg.from.id}): Nội dung của bạn chứa từ ngữ không mong muốn. Vui lòng không sử dụng những từ này.`;
-        bot.sendMessage(chatId, replyText, { reply_to_message_id: messageId, parse_mode: 'Markdown' });
-    }
+    action.checkMessage(msg);
 });
