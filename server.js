b5223047c5f0585f9a90531d2c30bcdd4774ebb5
donbadao <22314385+donbadao@users.noreply.github.com>
2023-10-07 09:06:38 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-console.log("Server starting....");
+const TelegramBot = require('node-telegram-bot-api');
+const token = '6683681793:AAEtk8pBXlpHfqofpjDVKEvkzfhQQjYjlGo';
+const bot = new TelegramBot(token, { polling: true });
+
+
+bot.onText(/\/hello/, (msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatID, "Hello World từ modules action");
+});
