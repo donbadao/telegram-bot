204b73037127336a3bf273db1ae06d2c57628591
donbadao <22314385+donbadao@users.noreply.github.com>
2023-10-08 13:55:05 +0700

    Thêm danh sách công ty

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
 const TelegramBot = require('node-telegram-bot-api');
 const token = '6506614832:AAGwXgk0w8Hn2iNiyvtEy63Av19qBQ8rmXM';
 const bot = new TelegramBot(token, { polling: true });
-
+const axios = require('axios');
 
 bot.onText(/\/hello/, (msg) => {
     const chatId = msg.chat.id;
     bot.sendMessage(chatId, "Hello World từ modules action");
 });
 
+bot.onText(/\/companies/, (msg => {
+    const chatID = msg.chat.id;
+    let result = "";
+    axios.get('https://econtract-app.000webhostapp.com/econtract/index.php').then(response => {
+        let companies = response;
+        for (let i = 0; i < 10; i++) {
+            result += `\n-Tên công ty : ${companies[i].company_name}
+            \n-Mã số thuế: ${companies[i].company_taxcode}
+                        \n-Địa chỉ: ${companies[i].company_address}
+                        \n-Người đại diện: ${companies[i].company_owner}
+                        \n------------------------------------------------------------------------------------------`;
+        }
+    });
+    bot.sendMessage(chatID, result);
+}));
+
 bot.on('message', (msg) => {
-	const badWordsList = ['chó', 'dmcs', 'phản động', 'cc'];
+    const badWordsList = ['chó', 'dmcs', 'phản động', 'cc'];
     const chatId = msg.chat.id;
     const messageId = msg.message_id;
     const text = msg.text;
