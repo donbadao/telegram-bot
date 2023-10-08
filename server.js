45c7a3819205b5d9d6c457ae7baafbdf20d93cf1
donbadao <22314385+donbadao@users.noreply.github.com>
2023-10-08 14:01:07 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -12,14 +12,15 @@ bot.onText(/\/companies/, (msg => {
     const chatID = msg.chat.id;
     let result = "";
     axios.get('https://econtract-app.000webhostapp.com/econtract/index.php').then(response => {
-        let companies = response.data;
-        for (let i = 0; i < 10; i++) {
-            result += `\n-Tên công ty : ${companies[i].company_name}
-            \n-Mã số thuế: ${companies[i].company_taxcode}
-                        \n-Địa chỉ: ${companies[i].company_address}
-                        \n-Người đại diện: ${companies[i].company_owner}
-                        \n------------------------------------------------------------------------------------------`;
-        }
+        console.log(response.data[0]);
+        // let companies = response.data;
+        // for (let i = 0; i < 10; i++) {
+        //     result += `\n-Tên công ty : ${companies[i].company_name}
+        //     \n-Mã số thuế: ${companies[i].company_taxcode}
+        //                 \n-Địa chỉ: ${companies[i].company_address}
+        //                 \n-Người đại diện: ${companies[i].company_owner}
+        //                 \n------------------------------------------------------------------------------------------`;
+        // }
     });
     bot.sendMessage(chatID, result);
 }));
